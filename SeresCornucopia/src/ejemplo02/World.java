5b39c9269c9956608188af93e9eabb453efbc941
FJavierDam <59649266+FJavierDam@users.noreply.github.com>
2021-10-11 17:57:02 +0200

    .

---
+++ SeresCornucopia/src/ejemplo02/World.java
@@ -0,0 +1,67 @@
+package ejemplo02;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import jdk.nashorn.api.tree.ForInLoopTree;
+
+public class World {
+	private List<Being> beings = new ArrayList<>();
+	private final int MAXBEINGS = 5;
+	private Cornucopia cornucopia = Cornucopia.getInstance();
+	private boolean fullWorld = false;
+	private WorldView view;
+
+	public World(WorldView view) {
+		super();
+		this.view = view;
+	}
+
+	public void makeHistory() {
+		beings.add(new Being(cornucopia));
+		do {
+			for (int i = 0; i < MAXBEINGS; i++) {
+				beings.add(new Being(cornucopia));
+			}
+			for (Iterator iterator = beings.iterator(); iterator.hasNext();) {
+				Being being = (Being) iterator.next();
+				being.start();
+			}
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		} while (isAnyoneIn());
+		cornucopia.setEnable(false);
+		System.out.println("El world is over");
+	}
+
+	public boolean isFullWorld() {
+		return fullWorld;
+	}
+
+	public void setFullWorld(boolean fullWorld) {
+		this.fullWorld = fullWorld;
+	}
+
+	private boolean isAnyoneIn() {
+		return beings.size() > 0;
+	}
+
+	public List<Being> getBeings() {
+		return beings;
+	}
+
+	public long getConsumed() {
+		return cornucopia.getConsumed();
+	}
+
+	private boolean checkFullBox() {
+		return beings.size() >= MAXBEINGS;
+	}
+
+}
